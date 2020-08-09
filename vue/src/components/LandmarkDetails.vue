d57d3c4828c3840843410a3d406db5c92e42d2ef
Ryiam Al Attar <ryiamalattar@gmail.com>
2020-08-09 18:21:41 -0400

    progressss

--- vue/src/components/LandmarkDetails.vue
+++ vue/src/components/LandmarkDetails.vue
@@ -5,23 +5,24 @@
       rel="stylesheet"
     >
     <div>
-      <button
+      <button class="button-itinerary button"
         v-on:click="showListOfItineraries = !showListOfItineraries"
       >{{showListOfItineraries === true ? "Cancel": "Add to Itineraries"}}</button>
     </div>
    
    
-    <tr v-show="showListOfItineraries">
+    <tr class="itTable" v-show="showListOfItineraries">
       <div v-for="itinerary in this.$store.state.itineraries" v-bind:key="itinerary.itineraryID">
         <td>{{itinerary.name}}</td>
         <td>
-        <input type="checkbox" v-bind:id="itinerary.itineraryID" v-bind:value="itinerary.name"  v-model="itineraryLandmark.itineraryId" :checked="addLandmarkToIt()"/>
+        <input type="checkbox" v-bind:id="itinerary.itineraryID"  v-on:click="addLandmarkToIt(itinerary.itineraryID)"/>
         </td>
       </div>
     </tr>
 
 
 <!-- v-if="showListOfItineraries === true" -->
+  <div class="grid">
     <div>
       <h1 class="title">{{this.$store.state.activeLandmark.name}}</h1>
       <p class="description">{{this.$store.state.activeLandmark.discription}}</p>
@@ -29,12 +30,12 @@
     <div>
       <img class="single-image" v-bind:src="getImageUrl(this.$store.state.activeLandmark.img)">
     </div>
-    <div>
+    <div class= "card">
       <div class="gallery gallery cf" v-for="singleImg in this.$store.state.activeLandmark.moreImgs" v-bind:key="singleImg.id">
         <img class="single-in-gallery-image" v-bind:src="getImageUrl(singleImg)">
       </div>
     </div>
-
+  </div>
   </div>
   
 </template>
@@ -69,7 +70,8 @@ export default {
       });
     },
   
-    addLandmarkToIt(){
+    addLandmarkToIt(id){
+      this.itineraryLandmark.itineraryId = id;
       LandmarkService.addLandmarkToItinerary(this.landmarkID, this.itineraryLandmark).then(response => {
         if(response.status === 201){
             alert("your landmark has been added");
@@ -103,6 +105,7 @@ export default {
 .single-image {
   height: 40rem;
   margin: 2rem;
+  grid-area: single-image;
 }
 
 .title {
@@ -116,6 +119,7 @@ export default {
   border: 1px solid saddlebrown;
   margin: 3rem;
   padding: 2rem;
+  grid-area: description;
 }
 
 .landmark-explained {
@@ -124,12 +128,23 @@ export default {
   flex-direction: row;
   justify-content: space-around;
 }
+.button-itinerary{
+  float:right;
+  position: absolute;
+  right: 10px;
+  top: 5px;
+}
+.itTable{
+  position: absolute;
+  right: 20px;
+  top: 80px;
+}
 
 
 
-/* this is for thw gallery */
+/* this is for the gallery */
 
-.gallery {
+/* .gallery {
   width: 790px;
   margin: 0 auto;
   padding: 5px;
@@ -146,12 +161,12 @@ export default {
   width: 250px;
   transition: 0.1s transform;
   transform: translateZ(0); /* hack */
-}
+/* }
 
 .gallery :hover {
-  z-index: 1;
+  z-index: 1; */
   /* position:relative; */
-}
+/* }
 
 .gallery :hover {
   transform: scale(5, 5);
@@ -174,5 +189,49 @@ export default {
   height: calc(var(--size) * 1.15);
   width: var(--size);
   position: center;
+} */
+
+/* h1{
+  grid-area: h1;
+} */
+
+
+/* .card {
+  grid-gap: 10px;
+  color: white;
+  filter:grayscale(25%);
+  font-family: 'Open Sans', sans-serif;
+  grid-area: card;
+}
+.card:hover {
+  
+  text-decoration: underline;
+  
+  filter: contrast(140%);
+  filter: saturate(150%);
+  
+} */
+.single-in-gallery-image{
+  --size: 400px;
+  height: calc(var(--size) * 1.15);
+  width: var(--size);
+  border-radius: calc(var(--size) * 0.15);
+  background-image: var(--img);
+  background-size: cover;
+  background-repeat: no-repeat;
+  
+}
+/* repeat(auto-fill, minmax(20rem, 1fr)); */
+.grid {
+  display: grid;
+  grid-template-columns: 
+  /* ". h1 h1 h1 h1" */
+  ". description single-image single-image ."
+  repeat(auto-fill, minmax(20rem, 1fr));
+
+  grid-gap: 1rem;
+  max-width: 80rem;
+  margin: 5rem auto;
+  padding: 10 5rem;
 }
 </style>
